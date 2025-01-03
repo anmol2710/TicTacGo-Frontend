b6aa98036d1ef8f39eea62b95926a784fe98c377
Anamika Garg <anamikagarg2906@gmail.com>
2025-01-03 18:15:08 +0530

    Home Page Basic UI

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,35 +1,43 @@
-import { useEffect, useState } from 'react'
-import '../App.css'
-import { io, Socket } from 'socket.io-client'
+import { useEffect, useState } from "react";
+import "../App.css";
+import { io, Socket } from "socket.io-client";
 
 function Home() {
-  const [socket , setSocket] = useState<Socket>()
-  
-  useEffect(() => {
-    const newSocket = io('http://localhost:3000')
-    setSocket(newSocket);
-    newSocket.on('connect', () => {
-      console.log('connected')
-    })
-    
-    newSocket.on("matchFound", (data) => {
-      console.log("Match Found")
-      console.log(data)
-    })
+  const [socket, setSocket] = useState<Socket>();
 
-    return () => {
-      newSocket.disconnect();
-    };
-  }, [])
-  
+  // useEffect(() => {
+  //   const newSocket = io('http://localhost:3000')
+  //   setSocket(newSocket);
+  //   newSocket.on('connect', () => {
+  //     console.log('connected')
+  //   })
+
+  //   newSocket.on("matchFound", (data) => {
+  //     console.log("Match Found")
+  //     console.log(data)
+  //   })
+
+  //   return () => {
+  //     newSocket.disconnect();
+  //   };
+  // }, [])
+
   function findMatch() {
-    console.log("findMatch")
-    if (socket) { 
-      socket.emit('findMatch')
+    console.log("findMatch");
+    if (socket) {
+      socket.emit("findMatch");
     }
   }
   return (
     <>
-      <button onClick={findMatch}>Find Match</button>     </>
-  )
-} export default Home
+    <div className="container flex flex-col gap-6">
+      <h1 className="font-extrabold">Play Tic Tac Go!</h1>
+
+      <button onClick={findMatch} className="mt-4 p-4 text-xl">Play With a Friend</button>
+      <button onClick={findMatch} className="p-4 text-xl" >Play with Bot</button>
+      <button onClick={findMatch} className="p-4 text-xl" >Play Online with Random Player</button>
+      </div>
+    </>
+  );
+}
+export default Home;
